d8476854eb236df58372ef8da69b7c6a741ab78a
Anni Favela <afavel5@illinois.edu>
2020-11-07 10:10:54 -0600

    Add Maps SDK for iOS CocoaPods  have to use Workspace

---
+++ Podfile.swift
@@ -0,0 +1,15 @@
+//
+//  Podfile.swift
+//  RodaxTravel
+//
+//  Created by Student on 11/7/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import Foundation
+
+source 'https://github.com/CocoaPods/Specs.git'
+target 'YOUR_APPLICATION_TARGET_NAME_HERE' do
+  pod 'GoogleMaps', '4.3.0'
+  pod 'GooglePlaces', '4.3.0'
+end
