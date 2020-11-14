bec5f64197bb23c08d67ac5d71b429f906e86f1a
Clare Favela <favelacl@grinnell.edu>
2020-11-14 16:26:37 -0600

    Fixed TabBarSegue Issue, Added button to Covid info page, Added Upload Image code to profile page

--- RodaxTravel/CovidInfoViewController.swift
+++ RodaxTravel/CovidInfoViewController.swift
@@ -11,22 +11,25 @@ import WebKit
 
 class CovidInfoViewController: UIViewController {
 
-        private let button: UIButton = {
+        /*private let button: UIButton = {
             let button = UIButton()
             button.setTitle("Show WebPage", for: .normal)
             button.backgroundColor = .link
             button.setTitleColor(.white, for: .normal)
             return button
-        }()
+        }()*/
+    
+    @IBOutlet weak var onMoreInfoPressed: UIButton!
+    
         
         @IBOutlet var webview: WKWebView!
         
         override func viewDidLoad() {
             super.viewDidLoad()
-            view.addSubview(button)
-            button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-            button.frame = CGRect(x: 0, y: 0, width: 220, height: 50)
-            button.center = view.center
+            view.addSubview(onMoreInfoPressed)
+            onMoreInfoPressed.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+            onMoreInfoPressed.frame = CGRect(x: 0, y: 0, width: 220, height: 50)
+            onMoreInfoPressed.center = view.center
             // Do any additional setup after loading the view.
     //        webview.load(URLRequest(url: URL(string: "https://www.google.com")!))
         }
