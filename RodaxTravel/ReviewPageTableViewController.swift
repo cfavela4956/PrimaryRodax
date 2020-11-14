587b49154cdc64923e0cbd801f0e32ea45e38aef
Clare Favela <favelacl@grinnell.edu>
2020-11-14 16:32:39 -0600

    Added jonathan's work: review page

---
+++ RodaxTravel/ReviewPageTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ReviewPageTableViewController.swift
+//  RodaxTravel
+//
+//  Created by Student on 11/14/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ReviewPageTableViewController: UITableViewController {
+
+
+        @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+        @IBOutlet weak var saveBarButton: UIBarButtonItem!
+        @IBOutlet weak var nameLabel: UILabel!
+        @IBOutlet weak var addressLabel: UILabel!
+        @IBOutlet weak var postedByLabel: UILabel!
+        @IBOutlet weak var starBackgroundColor: UIView!
+        @IBOutlet weak var reviewTitleField: UITextField!
+        @IBOutlet weak var reviewDateLabel: UILabel!
+        @IBOutlet weak var reviewTextView: UITextView!
+        @IBOutlet weak var deleteButton: UIButton!
+        
+        
+        @IBOutlet var starButtonCollection: [UIButton]!
+
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+        }
+
+        func leaveViewController() {
+                let isPresentingInAddMode = presentingViewController is UINavigationController
+                if isPresentingInAddMode {
+                    dismiss(animated: true, completion: nil)
+                } else {
+                    navigationController?.popViewController(animated: true)
+                }
+            }
+            
+            
+            
+            @IBAction func reviewTitleChange(_ sender: UITextField) {
+            }
+            
+            @IBAction func reviewTitleDonePressed(_ sender: UITextField) {
+            }
+            
+            @IBAction func deleteButtonPressed(_ sender: UIButton) {
+            }
+            @IBAction func cancelButtonPressed(_ sender: Any) {
+                leaveViewController()
+            }
+            @IBAction func saveButtonPressed(_ sender: Any) {
+            }
+
+    
+
+}
