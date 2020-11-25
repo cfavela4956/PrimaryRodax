179eeda557adecc935ab1e0162a750f50f796238
JonathanLanda <landajonathan.jl@gmail.com>
2020-11-25 14:08:09 -0600

    Added wishlist table cell

---
+++ RodaxTravel/WishlListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WishlListTableViewCell.swift
+//  RodaxTravel
+//
+//  Created by Student on 11/20/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class WishlListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
