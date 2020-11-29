e1d6e7b75a63f8caf39e02a937f3d8013cea6fe3
Anni Favela <afavel5@illinois.edu>
2020-11-29 14:45:11 -0600

    Adding Chat feature

---
+++ RodaxTravel/Chat Feature/ConversationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConversationViewController.swift
+//  RodaxTravel
+//
+//  Created by Student on 11/29/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .red
+        }
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+            
+            if !isLoggedIn {
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.modalPresentationStyle = .fullScreen
+                present(nav, animated: false)
+            }
+        }
+
+    }
