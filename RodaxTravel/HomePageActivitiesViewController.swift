c9c51d9483e3797a3d0617158df7e307dc868e35
Clare Favela <favelacl@grinnell.edu>
2020-11-29 14:01:50 -0600

    Add app icon on simulator

--- RodaxTravel/HomePageActivitiesViewController.swift
+++ RodaxTravel/HomePageActivitiesViewController.swift
@@ -8,6 +8,54 @@
 
 import UIKit
 
-class HomePageActivitiesViewController: ViewController {
+class HomePageActivitiesViewController: UIViewController {
+    
+        @IBOutlet weak var view1: UIView!
+        @IBOutlet weak var view2: UIView!
+        @IBOutlet weak var view3: UIView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+        @IBAction func segmentedControlChanged(_ sender: Any) {
+
+        func showView1() {
+            view1.isHidden = false
+            view2.isHidden = true
+            view3.isHidden = true
+        }
+
+        func showView2() {
+            view1.isHidden = true
+            view2.isHidden = false
+            view3.isHidden = true
+        }
+            
+        func showView3() {
+            view1.isHidden = true
+            view2.isHidden = true
+            view3.isHidden = false
+        }
+
+        guard let segmentedControl = sender as?
+            UISegmentedControl else { return }
+        if segmentedControl.selectedSegmentIndex == 0 {
+            showView1()
+
+        }
+           else if segmentedControl.selectedSegmentIndex == 1 {
+                showView2()
+
+            }
+            else {
+                       showView3()
+                   }
+            
+            
+        }
+
+
 
 }
