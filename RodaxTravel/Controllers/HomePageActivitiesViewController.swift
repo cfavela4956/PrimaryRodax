0c1762bd76d6e0df2fae4ee3d5ba77b66302ac51
Clare Favela <favelacl@grinnell.edu>
2020-12-01 10:39:41 -0600

    Added new swift file for segmented controller and fixed constraints on webview page

---
+++ RodaxTravel/Controllers/HomePageActivitiesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageActivitiesViewController.swift
+//  RodaxTravel
+//
+//  Created by Student on 12/1/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class HomePageActivitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
