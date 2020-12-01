a7fb6d3ee2c75a2148b37c8b1800ccf408fbfec7
JonathanLanda <landajonathan.jl@gmail.com>
2020-12-01 15:10:54 -0600

    Added file for Wishlist scene.

---
+++ RodaxTravel/WishlistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WishlistTableViewCell.swift
+//  RodaxTravel
+//
+//  Created by Student on 12/1/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class WishlistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
